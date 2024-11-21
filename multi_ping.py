2732c9e644674636a6d1b687a0d23e0783126ba5
Cybra <creepcreep1314@gmail.com>
2024-11-21 17:45:15 +0800

    Format total time output to display two decimal places

--- multi_ping.py
+++ multi_ping.py
@@ -74,4 +74,4 @@ if __name__ == "__main__":
     start_time = time.time()
     main()
     end_time = time.time()
-    print(f"Total time taken: {end_time - start_time} seconds")
+    print(f"Total time taken: {end_time - start_time:.2f} seconds")
